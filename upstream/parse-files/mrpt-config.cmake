a643c02b603fc1bda57099391554e5f651232af8
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-08-18 21:10:09 +0200

    Revert "Trimmed the branch to only the mrpt_libros2bridge sub directory"
    This reverts commit a2035b14a8101ad105a972999838e025d4de4f38.

---
+++ upstream/parse-files/mrpt-config.cmake
@@ -0,0 +1,41 @@
+# =========================================================================
+#  The Mobile Robot Programming Toolkit (MRPT) CMake configuration file
+#
+#             ** File generated automatically, do not modify **
+#
+#  Please, read:
+#  https://docs.mrpt.org/reference/latest/mrpt_from_cmake.html
+#
+#   In your CMakeLists.txt, add these lines:
+#
+#    find_package(MRPT REQUIRED
+#       COMPONENTS slam nav
+#       OPTIONAL_COMPONENTS  vision)
+#    target_link_libraries(MY_TARGET_NAME ${MRPT_LIBRARIES})
+#
+#   or do it individually:
+#    find_package(mrpt-slam)
+#    find_package(mrpt-nav)
+#    target_link_libraries(MY_TARGET_NAME mrpt::slam mrpt::nav)
+# =========================================================================
+include(CMakeFindDependencyMacro)
+
+set(MRPT_LIBRARIES "")
+foreach(_comp ${MRPT_FIND_COMPONENTS})
+  # If not set, use current directory as guess:
+  if (NOT mrpt-${_comp}_DIR)
+    set(mrpt-${_comp}_DIR  ${CMAKE_CURRENT_LIST_DIR})
+  endif()
+
+  if (MRPT_FIND_REQUIRED_${_comp})
+    find_dependency(mrpt-${_comp})
+  else()
+    find_package(mrpt-${_comp} CONFIG QUIET)
+  endif()
+  if (mrpt-${_comp}_FOUND)
+    list(APPEND MRPT_LIBRARIES mrpt::${_comp})
+  endif()
+endforeach()
+
+# backwards-compatibility var name:
+set(MRPT_LIBS ${MRPT_LIBRARIES})
