1ac1fcac7c0b7d44d41b5beb23a0ce6655c652ae
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-08-18 20:47:59 +0200

    Revert "Trimmed the branch to only the mrpt_libbase sub directory"
    This reverts commit 8fce73e82796db06ba28ddd463d70b715e513f9c.

---
+++ upstream/libs/math/src/CSparse/cs_reach.c
@@ -0,0 +1,22 @@
+// CSparse/Source/cs_reach: find the reach of a set in an arbitrary graph
+// CSparse, Copyright (c) 2006-2023, Timothy A. Davis. All Rights Reserved.
+// SPDX-License-Identifier: LGPL-2.1+
+#include "cs.h"
+/* xi [top...n-1] = nodes reachable from graph of G*P' via nodes in B(:,k).
+ * xi [n...2n-1] used as workspace */
+int cs_reach (cs *G, const cs *B, int k, int *xi, const int *pinv)
+{
+    int p, n, top, *Bp, *Bi, *Gp ;
+    if (!CS_CSC (G) || !CS_CSC (B) || !xi) return (-1) ;    /* check inputs */
+    n = G->n ; Bp = B->p ; Bi = B->i ; Gp = G->p ;
+    top = n ;
+    for (p = Bp [k] ; p < Bp [k+1] ; p++)
+    {
+        if (!CS_MARKED (Gp, Bi [p]))    /* start a dfs at unmarked node i */
+        {
+            top = cs_dfs (Bi [p], G, top, xi, xi+n, pinv) ;
+        }
+    }
+    for (p = top ; p < n ; p++) CS_MARK (Gp, xi [p]) ;  /* restore G */
+    return (top) ;
+}
