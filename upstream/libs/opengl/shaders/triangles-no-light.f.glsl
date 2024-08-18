86127ea4434bd13669da0b24d3ea746b376b6ea7
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-08-18 20:30:18 +0200

    Revert "Trimmed the branch to only the mrpt_libopengl sub directory"
    This reverts commit 89f081b7d3922e654ce6360faabfbac992a114f5.

---
+++ upstream/libs/opengl/shaders/triangles-no-light.f.glsl
@@ -0,0 +1,14 @@
+R"XXX(#version 300 es
+
+// FRAGMENT SHADER: Default shader for MRPT CRenderizable objects
+// Jose Luis Blanco Claraco (C) 2019-2023
+// Part of the MRPT project
+
+in lowp vec4 frag_materialColor;
+out lowp vec4 color;
+
+void main()
+{
+    color = frag_materialColor;
+}
+)XXX"
